f8ef680249b7e98c2b7da71fe804d7f6a9cd2662
Reynardo Tjhin <reychen26@gmail.com>
2023-08-01 22:14:18 +1000

    Testing different features and Creating model training file

---
+++ src/models/train.py
@@ -0,0 +1,23 @@
+# do we need to do feature scaling (like normalisation, etc.) => check dataset
+
+# how should we split the training and test set?
+# k-fold cross-validation? stratify?
+
+# list of ML-based techniques classification
+classifiers = [
+    'SVC',
+    'ExtraTreesClassifier',
+    'LinearDiscriminantAnalysis',
+    'DecisionTreeClassifier',
+    'KNeighborsClassifier',
+    'RandomForestClassifier',
+    'MLPClassifier',
+    'AdaBoostClassifier',
+    'NuSVC',
+    'GaussianNB',
+    'QuadraticDiscriminantAnalysis'
+]
+
+# tuning hyperparameters? Are there automatic ways?
+
+# what evaluation metrics should we focus on?
